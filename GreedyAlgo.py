a7e29ac662c1baea60764d8720e16eaae295d115
Vigneshwaran R <107832637+vigneshwaran2110609@users.noreply.github.com>
2023-06-07 21:34:40 +0530

    Worked without seggregating

--- GreedyAlgo.py
+++ GreedyAlgo.py
@@ -1,3 +1,31 @@
+import pandas as pd
+dataset=pd.read_csv("C:\\Users\\vicky\\OneDrive\\Desktop\\Project_4th_sem\\RoomHive\\hotels_list.csv")
+List=[]
+s1=4000
+s2=5000
+ac="yes"
+f="yes"
+o=3
+for i in range(0,len(dataset)):
+    s=dataset['Name'].values[i]+dataset['Address'].values[i]
+    wei=1
+    profit=dataset['Price'].values[i]
+    if dataset['Price'].values[i]>s1 and dataset['Price'].values[i]<s2:
+        wei+=1
+    if dataset['AC'].values[i]==ac:
+        wei+=1
+    if dataset['Food'].values[i]==f:
+        wei+=1
+    if dataset['Occupancy'].values[i]==o:
+        wei+=1
+    if dataset['AC'].values[i]=="yes":
+        profit+=1
+    if dataset['Food'].values[i]=="yes":
+        profit+=1
+    if dataset['Occupancy'].values[i]=="yes":
+        profit+=1
+    lis=[wei,profit,s]
+    List.append(lis)
 class Item:
     def __init__(self, profit, weight, details):
         self.profit = profit
@@ -17,13 +45,18 @@ def Knapsack(W, arr):
             list_of_hotels.append(item.details)
 
     return list_of_hotels
-
 arr=[]
-l=[[60,10,"ABC"],[100,20,"XYZ"],[120,30,"MNO"]]
+cap=(s1+s2)/2
+if ac=="yes":
+    cap+=1
+if f=="yes":
+    cap+=1
+cap=cap+o
+cap=3*cap
 def ArrFun(arr,l):
     for i in l:
         arr.append(Item(i[0],i[1],i[2]))
     return arr
-arr=ArrFun(arr,l)
-max_val = Knapsack(50, arr)
+arr=ArrFun(arr,List)
+max_val = Knapsack(cap, arr)
 print(max_val)
