bf401eea649985773702cd242d2848f19c75194a
Vishwaa-Arumugam <127880126+Vishwaa-Arumugam@users.noreply.github.com>
2024-08-07 16:42:18 +0530

    Update test.py
    testing the api
    
    Co-Authored-By: Vishnu Siddharth V R <93894137+HallowSiddharth@users.noreply.github.com>

--- test.py
+++ test.py
@@ -18,3 +18,24 @@ def get_coordinates(place):
     else:
         print('Error occurred:', data['status'])
         return None
+    
+#     import requests
+
+
+# def get_coordinates(place):
+#     url = f'https://api.tomtom.com/search/2/geocode/{place}.json'
+
+#     params = {
+#         'key': 'aGArnm6oXVRAGqBfiFCNpZaktp6OewMl',
+#         'query': place
+#     }
+
+#     response = requests.get(url, params=params)
+#     data = response.json()
+
+#     if response.status_code == 200 and data['summary']['numResults'] > 0:
+#         coordinates = data['results'][0]['position']
+#         return coordinates
+#     else:
+#         print('Error occurred:', data['status'])
+#         return None
